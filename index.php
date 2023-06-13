5c8c3d1f49fc506197845a3bcd68d2cc264137f3
sarahcssiqueira <sarahcso@hotmail.com>
2023-06-13 11:35:09 -0300

    doc: plugin header updated

--- index.php
+++ index.php
@@ -1,14 +1,18 @@
 <?php
 /**
- * Plugin Name:       Basic Static Block
- * Author:            Sarah Siqueira
+ * Plugin Name:       Basic Block
+ * Plugin URI:        https://sarahjobs.com/wp-plugins/basic-block
  * Description:       Boilerplate to create a static Gutenberg block
  * Version:           1.0.0
- * Requires at least: 5.9
- * Requires PHP:      7.0
- * License:           ''
- * License URI:       ''
- * Text Domain:       basic gutenberg block
+ * Requires at least: 5.6
+ * Requires PHP:      7.4
+ * Author:            Sarah Siqueira
+ * Author URI:        https://sarahjobs.com/about
+ * License:           GPLv2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl.html
+ * Text Domain:       basic-block
+ * Domain Path:       /languages
+ * Update URI:        https://sarahjobs.com/wp-plugins/basic-block/update
  * 
  */
 
